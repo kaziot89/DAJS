017e7eff8767f1b6e08ba4fcea384696d3f6ef3a
Kazimierz Targowski <k.targowski@gmail.com>
2023-06-14 20:28:34 +0200

    database changes in js

--- home/addProduct.js
+++ home/addProduct.js
@@ -1,60 +1,62 @@
-const $saveProduct = document.getElementById("save");
-const $goBack = document.getElementById("back");
-
-$saveProduct.addEventListener("click", add);
-$goBack.addEventListener("click", ret);
-
-function ret() {
-  window.location.href = "index.html";
-}
-
-function add() {
-  let nameInput = document.getElementById("prodName");
-  let prodName = nameInput.value;
-
-  let db = new SQL.Database();
-  db.run("CREATE TABLE IF NOT EXISTS Dane (id INTEGER PRIMARY KEY, name TEXT)");
-  db.run("INSERT INTO Dane (name) VALUES (?)", [prodName]);
-  let dbData = db.export();
-  let blob = new Blob([dbData], { type: "application/octet-stream" });
-  saveAs(blob, "bazadanych.sqlite");
-}
-
 // const $saveProduct = document.getElementById("save");
 // const $goBack = document.getElementById("back");
 
-// $saveProduct.addEventListener("click", add);
+// // $saveProduct.addEventListener("click", add);
 // $goBack.addEventListener("click", ret);
 
 // function ret() {
 //   window.location.href = "index.html";
 // }
+const sqlite3 = require("sqlite3").verbose();
+
+// Łączenie z bazą danych
+const db = new sqlite3.Database("./products.db", (err) => {
+  if (err) {
+    console.error(err.message);
+    return;
+  }
+  console.log("Połączono z bazą danych SQLite.");
+});
+
+// Dodawanie nowego rekordu do tabeli Produkty
+const newProduct = {
+  name: "Nowy produkt",
+  price1: 12,
+  price2: 14,
+  price3: 16,
+};
+
+const sql = `INSERT INTO Produkty (name, price1, price2, price3) VALUES (?, ?, ?, ?)`;
+
+db.run(
+  sql,
+  [newProduct.name, newProduct.price1, newProduct.price2, newProduct.price3],
+  function (err) {
+    if (err) {
+      console.error(err.message);
+      return;
+    }
+    console.log(`Dodano nowy rekord do tabeli Produkty: ${this.lastID}`);
+  }
+);
+
+// Zamykanie połączenia z bazą danych
+db.close((err) => {
+  if (err) {
+    console.error(err.message);
+    return;
+  }
+  console.log("Zamknięto połączenie z bazą danych SQLite.");
+});
+
 // function add() {
-//   document
-//     .getElementById("save")
-//     .addEventListener("click", console.log("jsdkahsldf"), function (e) {
-//       e.preventDefault();
+//   let nameInput = document.getElementById("prodName");
+//   let prodName = nameInput.value;
 
-//       var form = e.target;
-//       var formData = new FormData(form);
-//       var data = {};
-
-//       // Konwertuj FormData na obiekt
-//       for (var pair of formData.entries()) {
-//         data[pair[0]] = pair[1];
-//       }
-
-//       // Zapisz dane do bazy danych (SQL.js)
-//       var db = new SQL.Database();
-//       db.run(
-//         "CREATE TABLE IF NOT EXISTS Dane (id INTEGER PRIMARY KEY, name TEXT)"
-//       );
-//       db.run("INSERT INTO Dane (name) VALUES (?,)", [data.prodName]);
-
-//       // Zapisz bazę danych do pliku
-//       var data = new Uint8Array(db.export());
-//       saveAs(new Blob([data]), "bazadanych.sqlite");
-//       var file = new Blob([data], { type: "application/octet-stream" });
-//       saveAs(file, "bazadanych.sqlite");
-//     });
+//   let db = new SQL.Database();
+//   db.run("CREATE TABLE IF NOT EXISTS Dane (id INTEGER PRIMARY KEY, name TEXT)");
+//   db.run("INSERT INTO Dane (name) VALUES (?)", [prodName]);
+//   let dbData = db.export();
+//   let blob = new Blob([dbData], { type: "application/octet-stream" });
+//   saveAs(blob, "bazadanych.db");
 // }
